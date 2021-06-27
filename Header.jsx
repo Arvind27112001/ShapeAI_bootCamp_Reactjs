3e548bf80915478dcf330af017db1f05a1838e0b
Arvind27112001 <86550385+Arvind27112001@users.noreply.github.com>
2021-06-27 14:28:20 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    
+      <header>
+      <h1> FREE BOOT CAMP SHAPEAI </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> JavaScript with Reactjs </h1>
+      <p>
+        
+        A Basic and fundamental Web development Bootcamp regarding Javascript
+        with Reactjs
+      </p>
+    </div>
+  );
+}
+
+export default Info;
